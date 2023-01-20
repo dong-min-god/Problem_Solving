1491cf3047923be38d878b78e1e609623c3bb531
dong-min-god <55154178+dong-min-god@users.noreply.github.com>
2023-01-20 18:19:23 +0900

    Delete 백준/Bronze directory

--- 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
+++
@@ -1,8 +0,0 @@
-#include <vector>
-using namespace std;
-long long sum(vector<int>& a) {
-	long long ans = 0;
-	for (vector<int>::iterator iter = a.begin(); iter < a.end(); iter++)
-		ans += *iter;
-	return ans;
-}
--- 백준/Bronze/4504. 배수 찾기/배수 찾기.py
+++
@@ -1,9 +0,0 @@
-num = int(input())
-while(1):
-    val = int(input())
-    if(val == 0):
-        break
-    if(val % num != 0):
-        print("%d is NOT a multiple of %d." %(val,num))
-    else:
-        print("%d is a multiple of %d." %(val,num))
